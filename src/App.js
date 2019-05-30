2f03652609852650e9726b53cd6262d4669befd2
minh <hoangducminh271196@gmail.com>
2019-05-30 18:21:47 +0700

    custom link

--- src/App.js
+++ src/App.js
@@ -4,29 +4,51 @@ import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
 
+const MenuLink =({label, to, activeOnlyWhenExact})=>{
+    return(
+        <Route
+            path = {to}
+            exact = {activeOnlyWhenExact}
+            children = {({match})=>{//đổi tượng match có nhiệm vụ xác định cái url hiện tại có trùng khớp với cái link của menu hay ko
+                var active = match ? 'active abc' : '';
+                return (
+                    <li className={active}>
+                        <Link to={to}>{label}</Link>
+                    </li>
+                )
+            }}
+        />
+    );
+}
+
 function App() {
     return (
         <Router>
             <div>
-                <nav className="navbar navbar-inverse">
+                <nav className="navbar navbar-default" role="navigation">
                     <ul className="nav navbar-nav">
-                        <li className="active">
+                        {/* <li className="active"> */}
                             {/* <Link to="/">Trang chủ</Link> */}
-                            <NavLink to="/" activeStyle={{}}>Trang chủ</NavLink>
-                        </li>
-                        <li>
+                            {/* activeStyle={{backgroundColor: '#080808', color: 'white'}} activeStyle*/}
+                            {/*  activeClassName="menu-active" activeClassName*/}
+                            {/* <NavLink exact to="/">Trang chủ</NavLink> */}
+                            <MenuLink label="Trang Chủ" to="/" activeOnlyWhenExact={true}/>
+                        {/* </li> */}
+                        {/* <li> */}
                             {/* <Link to="/about">Giới thiệu</Link> */}
-                            <NavLink to="/about">Giới thiệu</NavLink>
-                        </li>
-                        <li>
+                            {/* <NavLink to="/about">Giới thiệu</NavLink> */}
+                            <MenuLink label="Giới thiệu" to="/about" activeOnlyWhenExact={true}/>
+                        {/* </li> */}
+                        {/* <li> */}
                             {/* <Link to="/contact">Liên hệ</Link> */}
-                            <NavLink to="/contact">Liên hệ</NavLink>
-                        </li>
+                            {/* <NavLink to="/contact">Liên hệ</NavLink> */}
+                            {/* <MenuLink label="Liên hệ" to="/contact" activeOnlyWhenExact={true}/> */}
+                            {MenuLink({label:"Liên hệ" , to:"/contact", activeOnlyWhenExact:true})}
+                        {/* </li> */}
                     </ul>
                 </nav>
-                
-                <div class="container">
-                    <div class="row">
+                <div className="container">
+                    <div className="row">
                         <Route path="/" exact component={Home} />
                         <Route path="/home" exact component={Home} />
                         <Route path="/about" component={About} />
--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 function About() {
     return (
         <div>
-            Đây là trang About
+            Đây là trang giới thiệu
         </div>
     )
 }
